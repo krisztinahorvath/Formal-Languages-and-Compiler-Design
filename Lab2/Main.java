7479f09239062beb43b34b3463b70b17326f3340
913-Horvath-Krisztina <91715582+krisztinahorvath@users.noreply.github.com>
2023-10-22 22:50:56 +0300

    added v2 for symbol table implementation

--- Lab2/Main.java
+++ Lab2/Main.java
@@ -1,29 +1,31 @@
 public class Main {
     public static void main(String[] args) {
-        SymbolTable symbolTable = new SymbolTable(10);
+        SymbolTable identifiersSymbolTable = new SymbolTable(10);
+        SymbolTable constantsSymbolTable = new SymbolTable(10);
 
-        Pair<Integer, Integer> position1 = symbolTable.addIdentifier("variable1");
+        Pair<Integer, Integer> position1 = identifiersSymbolTable.addSymbol("variable1");
         System.out.println("id variable1 - " + position1);
 
-        Pair<Integer, Integer> position2 = symbolTable.addConstant("'1'"); // string constant
+        Pair<Integer, Integer> position2 = constantsSymbolTable.addSymbol("'1'"); // string constant
         System.out.println("const '1' - " + position2);
 
-        Pair<Integer, Integer> position3 = symbolTable.addConstant("1"); // integer constant
+        Pair<Integer, Integer> position3 = constantsSymbolTable.addSymbol("1"); // integer constant
         System.out.println("const 1 - " + position3);
 
-        System.out.println("const 'abc' - " + symbolTable.addConstant("'abc'")); // string constant
-        System.out.println("const '123' - " + symbolTable.addConstant("'123'")); // string constant
-        System.out.println("const 123 - " + symbolTable.addConstant("123")); // integer constant
-        System.out.println("id b - " + symbolTable.addIdentifier("b"));
+        System.out.println("const 'abc' - " + constantsSymbolTable.addSymbol("'abc'")); // string constant
+        System.out.println("const '123' - " + constantsSymbolTable.addSymbol("'123'")); // string constant
+        System.out.println("const 123 - " + constantsSymbolTable.addSymbol("123")); // integer constant
+        System.out.println("id b - " + identifiersSymbolTable.addSymbol("b"));
 
-        System.out.println(symbolTable);
+        System.out.println("Identifiers" +  identifiersSymbolTable);
+        System.out.println("Constants" + constantsSymbolTable);
 
         try {
-            assert symbolTable.getIdentifierPosition("variable1").equals(position1) : "variable1 doesn't have position " + position1;
-            assert symbolTable.getConstantPosition("'1'").equals(position2) : "string const 1 doesn't exist in the ST at position " + position2;
-            assert symbolTable.hasConstant("1"): "const 1 doesn't exist in the ST";
-            assert !symbolTable.hasIdentifier("a") : "identifier a does not exist in the ST";
-            assert symbolTable.hasConstant("'abc'") : "string const 'abc' does not exist in the ST";
+            assert identifiersSymbolTable.getSymbolPosition("variable1").equals(position1) : "variable1 doesn't have position " + position1;
+            assert constantsSymbolTable.getSymbolPosition("'1'").equals(position2) : "string const 1 doesn't exist in the ST at position " + position2;
+            assert constantsSymbolTable.hasSymbol("1"): "const 1 doesn't exist in the ST";
+            assert !identifiersSymbolTable.hasSymbol("a") : "identifier a does not exist in the ST";
+            assert constantsSymbolTable.hasSymbol("'abc'") : "string const 'abc' does not exist in the ST";
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
--- Lab2/SymbolTable.java
+++ Lab2/SymbolTable.java
@@ -1,42 +1,25 @@
 public class SymbolTable {
-    private HashTable<String> identifiersTable;
-    private HashTable<String> constantsTable;
+    private HashTable<String> symbolTable;
 
     public SymbolTable(int _nrBuckets){
-
-        identifiersTable = new HashTable<>(_nrBuckets);
-        constantsTable = new HashTable<>(_nrBuckets);
+        symbolTable = new HashTable<>(_nrBuckets);
     }
 
-    public Pair<Integer, Integer> addIdentifier(String identifier){
-        return identifiersTable.add(identifier);
+    public Pair<Integer, Integer> addSymbol(String identifier){
+        return symbolTable.add(identifier);
     }
 
-    public Pair<Integer, Integer> addConstant(String constant) {
-        return constantsTable.add(constant);
+    public boolean hasSymbol(String identifier){
+        return symbolTable.contains(identifier);
     }
 
-    public boolean hasIdentifier(String identifier){
-        return identifiersTable.contains(identifier);
-    }
-
-    public boolean hasConstant(String constant){
-        return constantsTable.contains(constant);
-    }
-
-    public Pair<Integer, Integer> getIdentifierPosition(String identifier){
-        return identifiersTable.getPosition(identifier);
-    }
-
-    public Pair<Integer, Integer> getConstantPosition(String constant){
-        return constantsTable.getPosition(constant);
+    public Pair<Integer, Integer> getSymbolPosition(String identifier){
+        return symbolTable.getPosition(identifier);
     }
 
     @Override
     public String toString(){
-        return "\nSymbolTable:" +
-                "\nidentifiersTable: " + identifiersTable +
-                "\nconstantsTable: " + constantsTable;
+        return "\nSymbolTable:" + symbolTable;
     }
 
 
