58268e0767674e692a2c64addf55761bf6d5d5e9
913-Horvath-Krisztina <91715582+krisztinahorvath@users.noreply.github.com>
2023-10-30 20:32:18 +0200

    fixed read problem

--- Lab3/src/LexicalAnalyzer.java
+++ Lab3/src/LexicalAnalyzer.java
@@ -62,14 +62,6 @@ public class LexicalAnalyzer {
         return -1;
     }
 
-    private String getReservedWord(int pos){
-        for(Pair<String, Integer> pair: reservedWords)
-            if(pair.getSecond().equals(pos))
-                return pair.getFirst();
-
-        return null;
-    }
-
     private int getSeparatorsCode(char sep){
         for(Pair<Character, Integer> pair: separators)
             if (pair.getFirst().equals(sep))
@@ -78,28 +70,12 @@ public class LexicalAnalyzer {
         return -1;
     }
 
-    private Character getSeparator(int pos){
-        for(Pair<Character, Integer> pair: separators)
-            if(pair.getSecond().equals(pos))
-                return pair.getFirst();
-
-        return null;
-    }
-
     private int getOperatorsCode(String op) {
         for (Pair<String, Integer> pair : operators)
             if (pair.getFirst().equals(op))
                 return pair.getSecond();
 
         return -1;
-    }
-
-    private String getOperator(int pos){
-        for(Pair<String, Integer> pair: operators)
-            if(pair.getSecond().equals(pos))
-                return pair.getFirst();
-
-        return null;
     }
 
     public void writeToFile(String inputFilePath) throws IOException {
@@ -114,26 +90,11 @@ public class LexicalAnalyzer {
         String pifFileName = "PIF" + fileNr + ".out";
         String stFileName = "ST" + fileNr + ".out";
 
-
         FileWriter pifFileWriter = new FileWriter("src\\files\\" + pifFileName);
         for (Pair<Integer, Pair<Integer, Integer>> pair: PIF){
-            String symbol = null;
-
-            if (pair.getFirst() == 1)
-                symbol = "id";
-            else if(pair.getFirst() == 2)
-                symbol = "const";
-            else if(pair.getFirst() >= 3 && pair.getFirst() <= 11)
-                symbol = String.valueOf(getSeparator(pair.getFirst()));
-            else if(pair.getFirst() >= 12 && pair.getFirst() <= 22)
-                symbol = getOperator(pair.getFirst());
-            else if(pair.getFirst() >= 23 && pair.getFirst() <= 31)
-                symbol = getReservedWord(pair.getFirst());
-
-            pifFileWriter.write(symbol + " -> " + "(" + pair.getSecond().getFirst() + ", " + pair.getSecond().getSecond() + ")\n");
+            pifFileWriter.write(pair.getFirst() + " -> " + "(" + pair.getSecond().getFirst() + ", " + pair.getSecond().getSecond() + ")\n");
         }
         pifFileWriter.close();
-
 
         FileWriter stFileWriter = new FileWriter("src\\files\\" + stFileName);
         stFileWriter.write("IdentifiersSymbolTable=" + identifiersSymbolTable);
@@ -142,7 +103,6 @@ public class LexicalAnalyzer {
         stFileWriter.close();
     }
 
-    // todo update documentation
     public String processFile(String filePath) throws IOException {
         clear(); // clear PIF and STs
 
@@ -151,7 +111,7 @@ public class LexicalAnalyzer {
         String identifierRegex = "^[A-Za-z][A-Za-z0-9]*$"; // must start with small/capital letter, can have any number
         Pattern identifierPattern = Pattern.compile(identifierRegex);
 
-        String integerConstantRegex = "^[+-]?[0-9]\\d*$";
+        String integerConstantRegex = "^[+-]?(?:0|[1-9]\\d*)$";
         Pattern integerConstantPattern = Pattern.compile(integerConstantRegex);
 
         String stringConstantRegex = "^\"[\\w\\d+\\-=. ?!]*\"$";
@@ -179,10 +139,10 @@ public class LexicalAnalyzer {
                     insideStringConst = true;
                     atom += character;
                 }
-                else{ // todo check if separator or operator
+                else{
                     int separatorCode = getSeparatorsCode((char) c);
-
                     if (separatorCode != -1){ // it is a separator
+
                         // check the previously formed atom, if it is a reserved word or an operator, add it to PIF with ST_pos (-1, -1)
                         int reservedWordCode = getReservedWordCode(atom);
                         if (reservedWordCode != -1){
@@ -207,11 +167,11 @@ public class LexicalAnalyzer {
                                     // add to PIF the identifier, code = 1 for ids
                                     PIF.add(new Pair<>(1, atomPosST));
                                 }
-                                else{ // check if it is a constant
+                                else{ // it is an integer constant
                                     matcher = integerConstantPattern.matcher(atom);
                                     Pair<Integer, Integer> atomPosST;
 
-                                    if (matcher.matches()){ // it is an integer constant
+                                    if (matcher.matches()){
                                         if (!constantsSymbolTable.hasSymbol(atom)) // doesn't exist we need to add it in the ST
                                             atomPosST = constantsSymbolTable.addSymbol(atom);
                                         else atomPosST = constantsSymbolTable.getSymbolPosition(atom);
@@ -219,9 +179,9 @@ public class LexicalAnalyzer {
                                         // add to PIF the identifier, code = 2 for const
                                         PIF.add(new Pair<>(2, atomPosST));
                                     }
-                                    else {
+                                    else { // it is a string constant
                                         matcher = stringConstantPattern.matcher(atom);
-                                        if (matcher.matches()){ // it is a string constant
+                                        if (matcher.matches()){
                                             if (!constantsSymbolTable.hasSymbol(atom)) // doesn't exist we need to add it in the ST
                                                 atomPosST = constantsSymbolTable.addSymbol(atom);
                                             else atomPosST = constantsSymbolTable.getSymbolPosition(atom);
